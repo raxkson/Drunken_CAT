c14ea6175468afb2195a9251f475fd9d795eb3dd
cady9812 <wsoh9812@naver.com>
2020-08-12 15:09:11 +0900

    Registration of acquaintances Class 구현 #6

--- app/src/main/java/com/example/drunken_cat/PersonAdd.java
+++ app/src/main/java/com/example/drunken_cat/PersonAdd.java
@@ -8,38 +8,26 @@ import android.widget.*;
 import java.util.*;
 import java.io.*;
 
-public class PersonAdd extends AppCompatActivity {
+
+public class AddFriend extends AppCompatActivity{
     HashSet<String> set = new HashSet<String>();
-    EditText et,et2;
+    private String mName, mPhoneNum;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-   //     et=findViewById(R.id.et); // front 구현 되면 주석제거
-    //    et2=findViewById(R.id.et2); // front 구현 되면 주석제거
-
+    public AddFriend(String Name, String PhoneNum) {
+        this.mName = Name;
+        this.mPhoneNum = PhoneNum;
     }
 
-    public void SaveData(View view) {
-        String name = et.getText().toString();
-        String phone = et2.getText().toString();
-        et.setText(""); et2.setText("");
-
-        DuplicationVerify(name,phone); // 중복검사
+    public void AddToLocalDB(){
 
         try {
             FileOutputStream fos = openFileOutput("Friend_Data.txt", MODE_APPEND);
             PrintWriter writer = new PrintWriter(fos);
 
-            Iterator iter = set.iterator();
-            while(iter.hasNext())
-                writer.write((String)iter.next() +"\n");
-
+            for(int i=0; i<3; i++)
+                writer.write(mName +" "+ mPhoneNum+"\n");
             writer.flush();
             writer.close();
-
-            //Toast.makeText(this, "saved", Toast.LENGTH_SHORT).show();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -48,26 +36,6 @@ public class PersonAdd extends AppCompatActivity {
 
     }
 
-
-   void DuplicationVerify(String n, String p){
-        String s = n+" "+p; set.add(s);
-
-       try {
-           FileInputStream InputStream = openFileInput("Friend_Data.txt");
-           InputStreamReader isr= new InputStreamReader(InputStream);
-           BufferedReader reader= new BufferedReader(isr);
-           StringBuffer buffer= new StringBuffer();
-
-           while(true){
-               String content= reader.readLine();
-               if(content==null)  break;
-               else set.add(content);
-           }
-       } catch (FileNotFoundException e) {e.printStackTrace();} catch (IOException e) {e.printStackTrace();}
-
-       File mfile = new File("Friend_Data.txt");
-       mfile.delete();
-  }
-
-
 }
+
+
