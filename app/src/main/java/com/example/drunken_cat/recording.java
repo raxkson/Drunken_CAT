25fb2e3d3de988625704461c5012c5b9e386b566
cady9812 <wsoh9812@naver.com>
2020-08-16 22:36:22 +0900

    Voice Recording 기능 구현(시작/중지/재생) - #13

--- app/src/main/java/com/example/drunken_cat/recording.java
+++ app/src/main/java/com/example/drunken_cat/recording.java
@@ -2,11 +2,14 @@ package com.example.drunken_cat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.VideoView;
+import android.widget.MediaController;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -18,6 +21,7 @@ import java.io.IOException;
 public class recording extends AppCompatActivity {
     MediaRecorder record;
     String filename="";
+    MediaPlayer player;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recording);
@@ -28,12 +32,23 @@ public class recording extends AppCompatActivity {
 
         Log.d("recording","Save file : " + filename);
 
+        findViewById(R.id.record_play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playAudio();
+            }
+        });
+
         findViewById(R.id.record_start).setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 startRecord();
             }
         });
-
+        findViewById(R.id.record_stop).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                stopRecord();
+            }
+        });
     }
 
     public void permission(){
@@ -59,4 +74,44 @@ public class recording extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+    private void stopRecord() {
+        if (record != null) {
+            record.stop();
+            record.release();
+            record = null;
+            Toast.makeText(this, "Voice Recording Stop...", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void playAudio() {
+        try {
+            player = new MediaPlayer();
+            System.out.println("[*] "+filename);
+            player.setDataSource(filename);
+            final VideoView videoView = (VideoView) findViewById(R.id.record_view);
+
+            videoView.setVideoPath(filename);
+
+            MediaController mediaController = new MediaController(this);
+            mediaController.setAnchorView(videoView);
+            mediaController.setPadding(0, 0, 0, 80); //상위 레이어의 바닥에서 얼마 만큼? 패딩을 줌
+            videoView.setMediaController(mediaController);
+
+            videoView.start();
+       //     player.prepare();
+        //    player.start();
+
+            Toast.makeText(this, "재생중...", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void closePlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+    }
+
+
+
 }
